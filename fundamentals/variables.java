c9c652d6674c06271167a63233cb0be50460f145
William Fernandes <wiilfern1910@outlook.com>
2025-06-03 10:58:40 -0300

    Simple Types

---
+++ fundamentals/variables.java
@@ -0,0 +1,15 @@
+package fundamentals;
+
+public class variables {
+    public static void main(String[] args) {
+
+        int value = 2;
+        double dval = 2.5;
+        boolean testBoolean = false;
+        char charac = 't';
+        String name = "wila";
+        System.out.println("w");
+    }
+
+}
+
