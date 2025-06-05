8cbaa9a026e6d38b7e987f9c6595ccb8f083eb42
William Fernandes <wiilfern1910@outlook.com>
2025-06-05 10:40:14 -0300

    Upgraded Question

--- fundamentals/stringMethods.java
+++ fundamentals/stringMethods.java
@@ -4,13 +4,20 @@ import java.util.Scanner;
 public class stringMethods {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("What's your email: ");
-        String email = sc.nextLine();
-        int atIndex = email.indexOf("@");
-        String name = email.substring(0,atIndex);
-        String domain = email.substring(atIndex);
+        boolean containsAt = false;
+        while(!containsAt){
+            System.out.print("What's your email: ");
+            String email = sc.nextLine();
+            if (email.contains("@")) {
+                containsAt = true;
+                int atIndex = email.indexOf("@");
+                String name = email.substring(0, atIndex);
+                String domain = email.substring(atIndex);
+                System.out.printf("\nYour email name is %s and the domain you use is %s!", name, domain);
+            }
+        }
 
-        System.out.printf("\nYour email name is %s and the domain you use is %s!",name,domain);
+
 
         sc.close();
     }
