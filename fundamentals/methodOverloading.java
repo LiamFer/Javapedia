971adaf2bc67f5d0e606838bcf8f2197ee471d44
William Fernandes <wiilfern1910@outlook.com>
2025-06-05 16:39:33 -0300

    Testing Overloaded Function

---
+++ fundamentals/methodOverloading.java
@@ -0,0 +1,15 @@
+package fundamentals;
+
+public class methodOverloading {
+    public static void main(String[] args) {
+        System.out.println(multiply(2, 3));
+    }
+
+    static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+}
