d4af01298048a3f84705886f1f3f47d06c0d9da0
William Fernandes <wiilfern1910@outlook.com>
2025-06-06 15:17:49 -0300

    Static & Overloading

--- fundamentals/Student.java
+++ fundamentals/Student.java
@@ -1,17 +1,28 @@
 package fundamentals;
 
 public class Student {
+    // Static
     static int students = 0;
     String name;
     int age;
     double gpa;
     boolean isEnrolled;
 
+    // Constructor
     Student(String name, int age, double gpa, boolean isEnrolled) {
         this.name = name;
         this.age = age;
         this.gpa = gpa;
         this.isEnrolled = isEnrolled;
+        students+=1;
+    }
+
+    // Constructor Overloading
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.gpa = 0;
+        this.isEnrolled = true;
         students+=1;
     }
 
