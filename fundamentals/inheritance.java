1312f843fd59bec2b23724453d8e3515211ee434
William Fernandes <wiilfern1910@outlook.com>
2025-06-07 15:56:47 -0300

    Method Overriding

--- fundamentals/inheritance.java
+++ fundamentals/inheritance.java
@@ -2,15 +2,22 @@ package fundamentals;
 
 public class inheritance {
     public static void main (String[] args){
-        Soldier guts = new Soldier("Guts",20,"Ballista");
-        Soldier griffith = new Soldier("Griffith",19,"Crossbow");
 
         Soldier[] hawks = new Soldier[2];
+        Archer[] archSquad = new Archer[2];
+        Soldier guts = new Soldier("Guts",20,"Dragonslayer");
+        Soldier griffith = new Soldier("Griffith",19,"Rapier");
+        Archer linus = new Archer("Linus",20,"Balestra");
+        Archer legolas = new Archer("Legolas",322,"Bow");
         hawks[0] = griffith;
         hawks[1] = guts;
+        archSquad[0] = linus;
+        archSquad[1] = legolas;
 
-        for(Soldier soldier: hawks){
-            soldier.attack();
+        for(int i = 0; i < hawks.length;i++){
+            hawks[i].attack();
+            archSquad[i].attack();
         }
+
     }
 }
