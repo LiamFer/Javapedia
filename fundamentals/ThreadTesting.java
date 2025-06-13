2e397bb53d3ae350973b43e4fe4149b3f26e0870
William Fernandes <wiilfern1910@outlook.com>
2025-06-13 15:20:39 -0300

    Test

--- fundamentals/ThreadTesting.java
+++ fundamentals/ThreadTesting.java
@@ -7,7 +7,6 @@ public class ThreadTesting {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         Thread thread = new Thread(new TestThread(5));
 
-        
         CompletableFuture <String> t = CompletableFuture.supplyAsync(new TestSupply()).thenApply(text -> {
             System.out.println(text);
             return "";
