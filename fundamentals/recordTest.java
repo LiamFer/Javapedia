e57fcde623749f54432ae6b371d6a6d2dcf5bccf
William Fernandes <wiilfern1910@outlook.com>
2025-06-13 12:31:00 -0300

    Optional Return Type

--- fundamentals/recordTest.java
+++ fundamentals/recordTest.java
@@ -1,9 +1,18 @@
 package fundamentals;
 
+import java.util.Optional;
+
 public class recordTest {
     public static void main (String[] args){
-        Empregado e = new Empregado("William",2);
-        System.out.println(e);
+        Optional<Empregado> employee = findEmployee();
+        if(employee.isPresent()){
+            Empregado e = employee.get();
+            System.out.println(e);
+        }
+    }
 
+    public static Optional<Empregado> findEmployee(){
+        Empregado e = new Empregado("William",19);
+        return Optional.ofNullable(e);
     }
 }
